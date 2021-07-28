1d7274540078b85e314c849e079641c05d417bb9
pedfav <pedfav@gmail.com>
2021-07-27 22:50:06 -0300

    tempo

---
+++ tempo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import Adafruit_DHT
+from datetime import datetime
+
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+while True:
+  humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+  if humidity is not None and temperature is not None:
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    print(f{"{now} - Temperature={temperature} - Humidity={humidity}"})
+  else:
+    print("Fail to get data from dht22")
+
+  time.sleep(10)
