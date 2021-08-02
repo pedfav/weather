9b6f6c9a51a3ee6552c37d1fb9d34dd18544a850
pedfav <pedfav@gmail.com>
2021-08-02 17:29:12 -0300

    ouhiurhf

--- tempo.py
+++ tempo.py
@@ -4,6 +4,8 @@ import time
 from datetime import datetime
 import RPi.GPIO as GPIO
 from RPLCD.gpio import CharLCD
+import socket
+import requests
 
 GPIO.setwarnings(False)
 lcd = CharLCD(pin_rs=19, pin_rw=None, pin_e=16, pins_data=[21,18,23,24], numbering_mode=GPIO.BOARD, cols=16, rows=2, dotsize=8)
@@ -16,14 +18,40 @@ while True:
 
   if humidity is not None and temperature is not None:
     lcd.clear()
-    now = datetime.now().strftime("%H:%M:%S")
+    now = datetime.now().strftime("%d/%m/%Y %H:%M")
 
-    lcd.write_string(f"Time={now}")
+    lcd.write_string(now)
     lcd.cursor_pos=(1,0)
     lcd.write_string(f"T={round(temperature, 2)} - H={round(humidity, 2)}")
     print(f"Time={now} - Temperature={round(temperature, 2)} - Humidity={round(humidity, 2)}")
 
+    time.sleep(4)
+
+    lcd.clear()
+    now = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    lcd.write_string(now)
+    lcd.cursor_pos=(1,0)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    lcd.write_string(f"IP={s.getsockname()[0]}")
+    print(f"IP={s.getsockname()[0]}")
+
+    time.sleep(4)
+
+    lcd.clear()
+    now = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+    lcd.write_string(now)
+    lcd.cursor_pos=(1,0)
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+    btc = data["bpi"]["USD"]["rate"]
+    lcd.write_string(f"BTC={btc}")
+    print(f"BTC={btc}")
+
   else:
     print("Fail to get data from dht22")
 
-  time.sleep(2)
+  time.sleep(4)
